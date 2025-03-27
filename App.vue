9cc5f3f8d55dfeaa2849cc0932e7d1be5413a1df
Aditya Sharma <adityasharma96655@gmail.com>
2025-03-27 17:41:18 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import Home from './components/Home.vue';
+
+</script>
+
+<template>
+  <RouterView/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
