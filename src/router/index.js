7402ee108444c850f2b0bfb18d9c84c9db1efacb
Aditya Sharma <adityasharma96655@gmail.com>
2025-03-28 15:43:35 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name:'login',
+      component:()=>import('../components/login.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: () => import('@/components/Home.vue'),
