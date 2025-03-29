2baf45842b32a0b766fff597c3ef2f100c829414
Aditya Sharma <adityasharma96655@gmail.com>
2025-03-29 16:11:02 +0530

    Update Position.vue

--- src/views/Position.vue
+++ src/views/Position.vue
@@ -49,29 +49,12 @@
     </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        positions: [
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "angel (A537412)", script: "NIFTY15FEB2420000PE(NFO)", product: "INTRADAY", side: "SELL", quantity: 500, buyPrice: "09:13:54@ 0", sellPrice: "09:13:54@ 0", status: "CLOSED", pnl: 0 },
-          { strategy: "DOMAIN PRICE ACTION(BANKNIFTY)", broker: "angel (A537412)", script: "NIFTY15FEB2422000CE(NFO)", product: "INTRADAY", side: "BUY", quantity: 500, buyPrice: "09:13:54@ 0", sellPrice: "09:13:54@ 7", status: "CLOSED", pnl: 0 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "angel (A537412)", script: "NIFTY15FEB2420000PE(NFO)", product: "INTRADAY", side: "SELL", quantity: 551, buyPrice: "-", sellPrice: "09:13:54@ 5", status: "OPEN", pnl: 1653 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "angel (A537412)", script: "NIFTY15FEB242000000PE(NFO)", product: "INTRADAY", side: "BUY", quantity: 51, buyPrice: "09:13:54@ 1.2", sellPrice: "-", status: "OPEN", pnl: 0 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "iifl (AB046230)", script: "SBIN-EQ(NSE)", product: "INTRADAY", side: "BUY", quantity: 10, buyPrice: "09:13:54@ 800", sellPrice: "-", status: "OPEN", pnl: 0 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "iifl (AB046230)", script: "SBIN-EQ(NSE)", product: "INTRADAY", side: "BUY", quantity: 10, buyPrice: "09:13:54@ 810.95", sellPrice: "09:13:54@ 800", status: "CLOSED", pnl: -109.5 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "iifl (AB046230)", script: "SBIN-EQ(NSE)", product: "INTRADAY", side: "BUY", quantity: 1, buyPrice: "09:13:54@ 813.25", sellPrice: "-", status: "OPEN", pnl: 0 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "iifl (AB046230)", script: "SBIN-EQ(NSE)", product: "INTRADAY", side: "BUY", quantity: 1, buyPrice: "09:13:54@ 812.1", sellPrice: "09:13:54@ 0", status: "CLOSING", pnl: 0 },
-          { strategy: "DOMAIN TREND(NIFTY)", broker: "iifl (AB046230)", script: "SBIN-EQ(NSE)", product: "MIS", side: "BUY", quantity: 1, buyPrice: "09:13:54@ 802.75", sellPrice: "09:13:54@ 802.1", status: "CLOSED", pnl: -0.65 },
-        ],
-      };
-    },
-    computed: {
-      totalProfit() {
-        return this.positions.reduce((sum, trade) => sum + trade.pnl, 0);
-      },
-    },
-  };
+  <script setup>
+import { usePositionsStore } from '@/stores/PositionStore';
+import { storeToRefs } from 'pinia';
+
+  const position = usePositionsStore()
+  const {positions,totalProfit} = storeToRefs(position)
   </script>
   
   <style>
